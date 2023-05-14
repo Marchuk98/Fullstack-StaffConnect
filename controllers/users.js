110e1e57e0fbee16afafc0e2999fd29069ffb04b
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-05-14 23:56:27 +0300

    Login and password processing

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,32 @@
+const {prisma} = require("../prisma/prisma-client");
+const bryc = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
 const login = async (req, res, next) => {
     res.send('login')
+
+    if(!email && password){
+        return res.status(400).json({message:'Пожалуйста, заполните обязательное поля'})
+    }
+    const user = await prisma.user.findFirst({
+        where:{
+            email,
+        }
+    })
+    const isPasswordCorrect = user && (await bryc.compare(password, user.password))
+
+    if(user && isPasswordCorrect){
+        res.status(200).json({
+            id:user.id,
+            email:user.email,
+            name:user.name,
+        })
+    }else {
+        return res.status(400).json({message:"Не верно введён логин или пароль"})
+    }
 }
+
+
 const register = async (req, res, next) => {
     res.send('register')
 }
