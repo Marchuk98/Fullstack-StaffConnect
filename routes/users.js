6f769418fc41a5a9f697c32dc92e8b696e60b332
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-05-14 21:40:11 +0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {login, register, current} = require("../controllers/users");
+const router = express.Router();
+
+/* GET users listing. */
+router.post('/login', login);
+
+router.post('/register', register);
+
+router.get('/current', current);
+
+
+module.exports = router;
